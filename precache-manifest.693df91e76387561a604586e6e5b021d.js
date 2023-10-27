b6d5757e28461cae13fb289d1d3ef9000ff78a05
Rongjun Ma <rongjun.ma@aalto.fi>
2023-10-27 16:27:22 +0300

    Updates

---
+++ precache-manifest.693df91e76387561a604586e6e5b021d.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ca101da5db159926c8592f8a21e6a4f",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "e70515a05fb5ecd1678a",
+    "url": "/me/static/css/main.81ad51e5.chunk.css"
+  },
+  {
+    "revision": "d380ff89ca7d0d243eeb",
+    "url": "/me/static/js/2.bb076801.chunk.js"
+  },
+  {
+    "revision": "e70515a05fb5ecd1678a",
+    "url": "/me/static/js/main.c6469fd3.chunk.js"
+  },
+  {
+    "revision": "66be97a19e235fa95a6c",
+    "url": "/me/static/js/runtime~main.7f73a39c.js"
+  },
+  {
+    "revision": "d2c04dec58b355c2899137372b93296a",
+    "url": "/me/static/media/BI1.d2c04dec.png"
+  },
+  {
+    "revision": "03f18edf868f3ea4a2a78baaa32f7845",
+    "url": "/me/static/media/BI2.03f18edf.png"
+  },
+  {
+    "revision": "9dc269c8490dd638e376b3b03339fa93",
+    "url": "/me/static/media/BI3.9dc269c8.png"
+  },
+  {
+    "revision": "d6405f7db5cb20a09fd1a826f2fd0ea3",
+    "url": "/me/static/media/BI4.d6405f7d.png"
+  },
+  {
+    "revision": "27ac219b405a35ced6f932364f963905",
+    "url": "/me/static/media/CHI_present.27ac219b.JPG"
+  },
+  {
+    "revision": "319cd6cd563314ec57aada9cc7accb3f",
+    "url": "/me/static/media/Chester.319cd6cd.jpg"
+  },
+  {
+    "revision": "06acdd542d8007ce1792a46060edb110",
+    "url": "/me/static/media/nokia1.06acdd54.png"
+  },
+  {
+    "revision": "853098f99d7363fe5a57b0e2b48ff439",
+    "url": "/me/static/media/nokia2.853098f9.png"
+  },
+  {
+    "revision": "559b1292ae1e1dc2f68f2b01f165e7dc",
+    "url": "/me/static/media/nokia3.559b1292.png"
+  },
+  {
+    "revision": "d697816c9f0c61c764cd6222f6f1be26",
+    "url": "/me/static/media/p1.d697816c.jpg"
+  },
+  {
+    "revision": "c79ad7f17593c0eeec46a236ec2a197f",
+    "url": "/me/static/media/p2.c79ad7f1.jpg"
+  },
+  {
+    "revision": "73de3eac044b99f7af6989c551be8dde",
+    "url": "/me/static/media/p3.73de3eac.jpg"
+  },
+  {
+    "revision": "b3090acc338806f3beb870dbc96cfb35",
+    "url": "/me/static/media/p4.b3090acc.jpg"
+  },
+  {
+    "revision": "7597169f3fd5ddd8726fa8f3f46ba906",
+    "url": "/me/static/media/screenshots.7597169f.png"
+  },
+  {
+    "revision": "4ab87b874d73be96522796769aadbb8c",
+    "url": "/me/static/media/testa.4ab87b87.jpg"
+  }
+]);
